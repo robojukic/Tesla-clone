246ba6c255dffe5ba3efc6fdebebbf0763df46fd
Robo Jukić <robo.jukic2@gmail.com>
2023-03-26 20:29:58 +0200

    Add Header

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section(props) {
   return (
     <Wrap bgImage={props.imgUrl}>
-      <ItemText>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{props.title}</h1>
+          <p>{props.description}</p>
+        </ItemText>
+      </Fade>
+
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{props.leftBtn}</LeftButton>
-          {props.rightBtn && <RightButton>{props.rightBtn}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{props.leftBtn}</LeftButton>
+            {props.rightBtn && <RightButton>{props.rightBtn}</RightButton>}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
